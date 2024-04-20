376ee276e6de94e77c4a9535627e869fcf9bf782
Arferik <610231551@qq.com>
2024-04-20 09:31:40 +0800

    优化样式

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,11 +8,6 @@ const postsCollection = defineCollection({
     pubDate: z.date(),
     description: z.string(),
     author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    tags: z.array(z.string()),
   }),
 });
 // 导出一个单独的 `collections` 对象来注册你的集合
