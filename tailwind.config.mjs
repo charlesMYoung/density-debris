9787384fb44ab1bb68bf40044d6f39e322fc4a06
Arferik <610231551@qq.com>
2024-04-22 00:32:17 +0800

    feat: 新增hook

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,5 @@
-
 import plugin from "tailwindcss/plugin";
-import twTypography from '@tailwindcss/typography'
+import twTypography from "@tailwindcss/typography";
 
 const themeColors = () => {
 	return [
@@ -129,60 +128,64 @@ const typography = plugin(({ addComponents, theme }) => {
 	});
 });
 
-
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			colors: {
 				...themeColors(),
 			},
 			fontFamily: {
-				sans: ['smiley-sans'],
+				sans: ["smiley-sans"],
 			},
 			typography: ({ theme }) => {
 				return {
 					md: {
 						//tertiary
 						css: {
-							'--tw-prose-body': 'var(--md-sys-color-background)',
-							'--tw-prose-headings': 'var(--md-sys-color-primary)',
-							'--tw-prose-lead': 'var(--md-sys-color-secondary)',
-							'--tw-prose-links': 'var(--md-sys-color-primary)',
-							'--tw-prose-bold': 'var(--md-sys-color-primary)',
-							'--tw-prose-counters': 'var(--md-sys-color-secondary)',
-							'--tw-prose-bullets': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-hr': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-quotes': 'var(--md-sys-color-primary)',
-							'--tw-prose-quote-borders': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-captions': 'var(--md-sys-color-primary)',
-							'--tw-prose-code': 'var(--md-sys-color-primary)',
-							'--tw-prose-pre-code': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-pre-bg': 'var(--md-sys-color-primary)',
-							'--tw-prose-th-borders': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-td-borders': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-invert-body': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-invert-headings': theme('colors.white'),
-							'--tw-prose-invert-lead': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-invert-links': theme('colors.white'),
-							'--tw-prose-invert-bold': theme('colors.white'),
-							'--tw-prose-invert-counters': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-invert-bullets': 'var(--md-sys-color-secondary)',
-							'--tw-prose-invert-hr': 'var(--md-sys-color-primary)',
-							'--tw-prose-invert-quotes': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-invert-quote-borders': 'var(--md-sys-color-primary)',
-							'--tw-prose-invert-captions': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-invert-code': theme('colors.white'),
-							'--tw-prose-invert-pre-code': 'var(--md-sys-color-primary-container)',
-							'--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
-							'--tw-prose-invert-th-borders': 'var(--md-sys-color-secondary)',
-							'--tw-prose-invert-td-borders': 'var(--md-sys-color-primary)',
-						}
-					}
-				}
-			}
+							"--tw-prose-body": "var(--md-sys-color-background)",
+							"--tw-prose-headings": "var(--md-sys-color-primary)",
+							"--tw-prose-lead": "var(--md-sys-color-secondary)",
+							"--tw-prose-links": "var(--md-sys-color-primary)",
+							"--tw-prose-bold": "var(--md-sys-color-primary)",
+							"--tw-prose-counters": "var(--md-sys-color-secondary)",
+							"--tw-prose-bullets": "var(--md-sys-color-primary-container)",
+							"--tw-prose-hr": "var(--md-sys-color-primary-container)",
+							"--tw-prose-quotes": "var(--md-sys-color-primary)",
+							"--tw-prose-quote-borders":
+								"var(--md-sys-color-primary-container)",
+							"--tw-prose-captions": "var(--md-sys-color-primary)",
+							"--tw-prose-code": "var(--md-sys-color-primary)",
+							"--tw-prose-pre-code": "var(--md-sys-color-primary-container)",
+							"--tw-prose-pre-bg": "var(--md-sys-color-primary)",
+							"--tw-prose-th-borders": "var(--md-sys-color-primary-container)",
+							"--tw-prose-td-borders": "var(--md-sys-color-primary-container)",
+							"--tw-prose-invert-body": "var(--md-sys-color-primary-container)",
+							"--tw-prose-invert-headings": theme("colors.white"),
+							"--tw-prose-invert-lead": "var(--md-sys-color-primary-container)",
+							"--tw-prose-invert-links": theme("colors.white"),
+							"--tw-prose-invert-bold": theme("colors.white"),
+							"--tw-prose-invert-counters":
+								"var(--md-sys-color-primary-container)",
+							"--tw-prose-invert-bullets": "var(--md-sys-color-secondary)",
+							"--tw-prose-invert-hr": "var(--md-sys-color-primary)",
+							"--tw-prose-invert-quotes":
+								"var(--md-sys-color-primary-container)",
+							"--tw-prose-invert-quote-borders": "var(--md-sys-color-primary)",
+							"--tw-prose-invert-captions":
+								"var(--md-sys-color-primary-container)",
+							"--tw-prose-invert-code": theme("colors.white"),
+							"--tw-prose-invert-pre-code":
+								"var(--md-sys-color-primary-container)",
+							"--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+							"--tw-prose-invert-th-borders": "var(--md-sys-color-secondary)",
+							"--tw-prose-invert-td-borders": "var(--md-sys-color-primary)",
+						},
+					},
+				};
+			},
 		},
 	},
 	plugins: [typography, twTypography()],
-}
+};
