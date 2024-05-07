02b1e49e5838123d4cebd3e2212dcb3ff9ca5e63
y y <yy921010@qq.com>
2024-05-07 12:33:43 +0800

    build: 新增vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,16 @@ import react from "@astrojs/react";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    icon({
-      include: {
-        mdi: ["*"],
-        ri: ["*"],
-      },
-    }),
-    mdx(),
-  ],
+  integrations: [tailwind(), react(), icon({
+    include: {
+      mdi: ["*"],
+      ri: ["*"]
+    }
+  }), mdx()],
+  output: "server",
+  adapter: vercel()
 });
